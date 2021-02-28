a287c4c45512741571334875bd7cf4f5b66ec5a2
santosmarcelob <santos.marcelob@gmail.com>
2021-02-27 23:30:47 -0300

    update

--- lambda_expressions.py
+++
@@ -1,7 +0,0 @@
-#lambda expressions
-
-#lambda param: action(param)
-
-my_list = [1,2,3]
-
-print(list(map(lambda item: item*2, my_list)))
--- list_comprehensions.py
+++
@@ -1,9 +0,0 @@
-#list, set, dictionary
-
-#my_list =[param for param in iterable]
-my_list = [char for char in 'hello']
-my_list2 = [num for num in range(0,100)]
-my_list3 = [num**2 for num in range(0,100)]
-my_list4 = [num**2 for num in range(0,100) if num % 2 == 0]
-
-print(my_list4)
